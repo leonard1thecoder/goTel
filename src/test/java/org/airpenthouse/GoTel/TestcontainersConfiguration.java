06b5ec9b8d33f4ba9e02dd5555b4d5d3c9145af1
Sizolwakhe Mthimunye <leonard1geek@gmail.com>
2025-07-11 09:41:02 +0200

    adding TestcontainersConfiguration class

---
+++ src/test/java/org/airpenthouse/GoTel/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package org.airpenthouse.GoTel;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
