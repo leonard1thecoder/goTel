eefe091fcd8ea87f6743b8f4fc8efee3da3e6587
Sizolwakhe Mthimunye <leonard1geek@gmail.com>
2025-08-01 15:23:20 +0200

    added method to getCountryCode from the Country table for City and WorldLanguagesEntity who uses countryCode in queries for inserting values to DB

--- src/main/java/org/airpenthouse/GoTel/util/CommonEntityMethod.java
+++ src/main/java/org/airpenthouse/GoTel/util/CommonEntityMethod.java
@@ -1,25 +1,42 @@
 package org.airpenthouse.GoTel.util;
 
-
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 
-@NoArgsConstructor
-@Component
+import static org.airpenthouse.GoTel.util.Connect.getInstance;
+
 public class CommonEntityMethod {
 
+    private final String jdbcQueryFindCountryCode;
+
+    public CommonEntityMethod() {
+        jdbcQueryFindCountryCode = PropertiesUtilManager.getPropertiesValue("jdbc.query.findCountryCodeByCountry");
+    }
+
+    public String getCountryCodeByCountryName(String countryName) {
+        PreparedStatement ps;
+        try {
+            ps = databaseConfig(jdbcQueryFindCountryCode);
+            ps.setString(1, countryName);
+            ResultSet set = ps.executeQuery();
+            if (set.next()) {
+                return set.getString(1);
+            } else throw new RuntimeException("Country searched not found, entered : " + countryName);
+        } catch (SQLException | ExecutionException | TimeoutException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public PreparedStatement databaseConfig(String query) throws ExecutionException, InterruptedException, TimeoutException, SQLException {
-        Future<Connection> connect = Connect.getDB_data();
+        Future<Connection> connect = getInstance().getDB_data();
         assert connect != null;
         return connect.get(15, TimeUnit.SECONDS).prepareStatement(query);
     }
+
 }
