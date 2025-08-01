cd4575d581eb7472771b464fead92d7492a5b679
Sizolwakhe Mthimunye <leonard1geek@gmail.com>
2025-08-01 15:32:52 +0200

    amended the Entity classes

--- src/main/java/org/airpenthouse/GoTel/entities/country/CountriesEntity.java
+++ src/main/java/org/airpenthouse/GoTel/entities/country/CountriesEntity.java
@@ -1,55 +1,52 @@
 package org.airpenthouse.GoTel.entities.country;
 
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.airpenthouse.GoTel.util.CommonEntityMethod;
-
 import org.airpenthouse.GoTel.util.LOG;
 import org.airpenthouse.GoTel.util.PropertiesUtilManager;
-import org.springframework.stereotype.Component;
-
 import java.util.Set;
-import java.util.concurrent.ConcurrentSkipListSet;
 
-@Component
+@AllArgsConstructor
 public final class CountriesEntity implements Callable<Set<CountriesEntity>>, Comparable<CountriesEntity> {
 
-
+    private static CountriesEntity instance;
+    @Getter
     private String countryName;
-
+    @Getter
     private String countryRegion;
-
+    @Getter
     private int population;
 
 
     private Set<CountriesEntity> countries;
-    public static String ENTITY_TRIGGER = "FIND_ALL_COUNTRIES";
-    private String jdbcQueryFindAllCountries, jdbcQueryFindCountryCode, jdbcQueryFindCountryByName, jdbcQueryFindCountryByRegion, jdbcQueryFindCountryByContinent;
+    public static String ENTITY_TRIGGER;
+    private String jdbcQueryFindAllCountries, jdbcQueryFindCountryByName, jdbcQueryFindCountryByRegion, jdbcQueryFindCountryByContinent;
 
-    public CountriesEntity() {
+    private CountriesEntity() {
         super();
 
-        this.countries = new ConcurrentSkipListSet<>();
         jdbcQueryFindAllCountries = PropertiesUtilManager.getPropertiesValue("jdbc.query.allCountries");
         jdbcQueryFindCountryByName = PropertiesUtilManager.getPropertiesValue("jdbc.query.findCountryByName");
         jdbcQueryFindCountryByContinent = PropertiesUtilManager.getPropertiesValue("jdbc.query.findCountriesByContinent");
         jdbcQueryFindCountryByRegion = PropertiesUtilManager.getPropertiesValue("jdbc.query.findCountriesByRegion");
-        jdbcQueryFindCountryCode = PropertiesUtilManager.getPropertiesValue("jdbc.query.findCountryCodeByCountry");
-
-        LOG.info(jdbcQueryFindAllCountries);
     }
+
 
     static {
         commonEntityMethod = new CommonEntityMethod();
+    }
+
+    public static CountriesEntity getInstance() {
+        if (instance == null) {
+            instance = new CountriesEntity();
+        }
+        return instance;
     }
 
     private Set<CountriesEntity> getCountryByName() {
@@ -57,12 +54,16 @@ public final class CountriesEntity implements Callable<Set<CountriesEntity>>, Co
         try {
 
             ps = commonEntityMethod.databaseConfig(jdbcQueryFindCountryByName);
-            LOG.info("OK#######");
+            LOG.info(jdbcQueryFindCountryByName);
+            LOG.info("OK#######1");
             ps.setString(1, PropertiesUtilManager.getPropertiesValue("countryName"));
             ResultSet set = ps.executeQuery();
             return addDataFromDbToSet(set);
         } catch (SQLException | ExecutionException | TimeoutException | InterruptedException e) {
             throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
@@ -70,8 +71,7 @@ public final class CountriesEntity implements Callable<Set<CountriesEntity>>, Co
         PreparedStatement ps;
         try {
 
-            ps = commonEntityMethod.databaseConfig(jdbcQueryFindCountryByName);
-            LOG.info("OK#######");
+            ps = commonEntityMethod.databaseConfig(jdbcQueryFindCountryByContinent);
             ps.setString(1, PropertiesUtilManager.getPropertiesValue("continentName"));
             ResultSet set = ps.executeQuery();
             return addDataFromDbToSet(set);
@@ -84,8 +84,7 @@ public final class CountriesEntity implements Callable<Set<CountriesEntity>>, Co
         PreparedStatement ps;
         try {
 
-            ps = commonEntityMethod.databaseConfig(jdbcQueryFindCountryByName);
-            LOG.info("OK#######");
+            ps = commonEntityMethod.databaseConfig(jdbcQueryFindCountryByRegion);
             ps.setString(1, PropertiesUtilManager.getPropertiesValue("regionName"));
             ResultSet set = ps.executeQuery();
             return addDataFromDbToSet(set);
@@ -100,19 +99,7 @@ public final class CountriesEntity implements Callable<Set<CountriesEntity>>, Co
         return countryName.compareTo(obj.countryName);
     }
 
-    public Set<CountriesEntity> getCountryCodeFromDB() {
-        PreparedStatement ps;
-        try {
 
-            ps = commonEntityMethod.databaseConfig(jdbcQueryFindCountryCode);
-            LOG.info("OK#######");
-            ps.setString(1, PropertiesUtilManager.getPropertiesValue("countryName"));
-            ResultSet set = ps.executeQuery();
-            return null;
-        } catch (SQLException | ExecutionException | TimeoutException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 
     private Set<CountriesEntity> getAllCountries() {
@@ -132,7 +119,7 @@ public final class CountriesEntity implements Callable<Set<CountriesEntity>>, Co
     }
 
     @Override
-    public Set<CountriesEntity> call() throws Exception {
+    public Set<CountriesEntity> call() {
         return switch (ENTITY_TRIGGER) {
             case "FIND_ALL_COUNTRIES" -> getAllCountries();
             case "FIND_COUNTRY_BY_NAME" -> getCountryByName();
@@ -147,9 +134,13 @@ public final class CountriesEntity implements Callable<Set<CountriesEntity>>, Co
 
 
     private Set<CountriesEntity> addDataFromDbToSet(ResultSet set) throws SQLException {
+        countries = new CopyOnWriteArraySet<>();
         while (set.next()) {
+
             countries.add(new CountriesEntity(set.getString(2), set.getString(4), set.getInt(7)));
+
         }
+        System.out.println(countries);
         return countries;
     }
 
--- src/main/java/org/airpenthouse/GoTel/entities/languanges/WorldLanguagesEntity.java
+++ src/main/java/org/airpenthouse/GoTel/entities/languanges/WorldLanguagesEntity.java
@@ -1,73 +1,103 @@
 package org.airpenthouse.GoTel.entities.languanges;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.airpenthouse.GoTel.util.CommonEntityMethod;
+import org.airpenthouse.GoTel.util.LOG;
 import org.airpenthouse.GoTel.util.PropertiesUtilManager;
-import org.springframework.beans.factory.annotation.Autowired;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.concurrent.*;
 import java.util.Set;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+@AllArgsConstructor
 public class WorldLanguagesEntity implements Callable<Set<WorldLanguagesEntity>>, Comparable<WorldLanguagesEntity> {
     @Getter
-    private String country;
+    private String countryName;
     @Getter
-    private String language;
+    private String languageName;
     @Getter
     private Boolean languageStatus;
-
+    @Getter
+    private String newLanguageName;
+    private static WorldLanguagesEntity instance;
     private static final CommonEntityMethod commonEntityMethod;
-    private String queryFindAllLanguages, queryFindLanguagesByCountryName, queryFindLanguageByName;
-
-
+    private String queryFindAllLanguages, queryFindCityWithLanguageAndCountryName, queryFindLanguagesByCountryName, queryFindLanguageByName, updateLanguageName, updateLanguageStatus, insertLanguage;
     private Set<WorldLanguagesEntity> languages;
 
     static {
         commonEntityMethod = new CommonEntityMethod();
     }
 
-    @Override
-    public int compareTo(WorldLanguagesEntity obj) {
-        return language.compareTo(obj.language);
+    public static WorldLanguagesEntity getInstance() {
+        if (instance == null) {
+            instance = new WorldLanguagesEntity();
+        }
+        return instance;
     }
 
-    public WorldLanguagesEntity() {
+    @Override
+    public int compareTo(WorldLanguagesEntity obj) {
+        return languageName.compareTo(obj.languageName);
+    }
+
+    private WorldLanguagesEntity() {
         super();
-        languages = new CopyOnWriteArraySet<>();
+        queryFindCityWithLanguageAndCountryName = PropertiesUtilManager.getPropertiesValue("jdbc.query.findCityByCountryNameAndLanguage");
         queryFindAllLanguages = PropertiesUtilManager.getPropertiesValue("jdbc.query.allLanguage");
         queryFindLanguageByName = PropertiesUtilManager.getPropertiesValue("jdbc.query.findLanguageByName");
         queryFindLanguagesByCountryName = PropertiesUtilManager.getPropertiesValue("jdbc.query.findLanguageByCountryName");
-
+        updateLanguageName = PropertiesUtilManager.getPropertiesValue("jdbc.update.updateLanguageName");
+        updateLanguageStatus = PropertiesUtilManager.getPropertiesValue("jdbc.update.updateLanguageStatus");
+        insertLanguage = PropertiesUtilManager.getPropertiesValue("jdbc.add.addLanguage");
     }
 
     private WorldLanguagesEntity(String country, String language, Boolean languageStatus) {
-        this.country = country;
-        this.language = language;
+        this.countryName = country;
+        this.languageName = language;
         this.languageStatus = languageStatus;
     }
 
     public static String ENTITY_TRIGGER;
 
-    private Set<WorldLanguagesEntity> getAllLanguages() {
+    private Set<WorldLanguagesEntity> getCityByLanguageAndCountry(String languageName, String countryName) {
         try {
-            PreparedStatement ps = commonEntityMethod.databaseConfig(queryFindAllLanguages);
-            ResultSet set = ps.executeQuery();
-            return addDatFromDBToList(set);
+            preparedStatementFoRResultSet = commonEntityMethod.databaseConfig(queryFindCityWithLanguageAndCountryName);
+            preparedStatementFoRResultSet.setString(1, languageName);
+            preparedStatementFoRResultSet.setString(2, countryName);
+            return addDatFromDBToList(true);
         } catch (SQLException | TimeoutException | InterruptedException | ExecutionException e) {
             e.printStackTrace();
             return null;
         }
     }
 
+    private Set<WorldLanguagesEntity> getAllLanguages() {
+        try {
+            preparedStatementFoRResultSet = commonEntityMethod.databaseConfig(queryFindAllLanguages);
+
+            return addDatFromDBToList(true);
+        } catch (SQLException | TimeoutException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private boolean update;
     private Set<WorldLanguagesEntity> getLanguageByName() {
         try {
-            PreparedStatement ps = commonEntityMethod.databaseConfig(queryFindLanguageByName);
-            ps.setString(1, PropertiesUtilManager.getPropertiesValue("languageName"));
-            ResultSet set = ps.executeQuery();
-            return addDatFromDBToList(set);
+            preparedStatementFoRResultSet = commonEntityMethod.databaseConfig(queryFindLanguageByName);
+
+            if (update) {
+                preparedStatementFoRResultSet.setString(1, PropertiesUtilManager.getPropertiesValue("newLanguageName"));
+                update = false;
+            } else {
+                preparedStatementFoRResultSet.setString(1, PropertiesUtilManager.getPropertiesValue("languageName"));
+            }
+            return addDatFromDBToList(true);
         } catch (SQLException | TimeoutException | InterruptedException | ExecutionException e) {
             e.printStackTrace();
             return null;
@@ -76,10 +106,103 @@ public class WorldLanguagesEntity implements Callable<Set<WorldLanguagesEntity>>
 
     private Set<WorldLanguagesEntity> getLanguageByCountry() {
         try {
-            PreparedStatement ps = commonEntityMethod.databaseConfig(queryFindLanguagesByCountryName);
-            ps.setString(1, PropertiesUtilManager.getPropertiesValue("countryName"));
-            ResultSet set = ps.executeQuery();
-            return addDatFromDBToList(set);
+            preparedStatementFoRResultSet = commonEntityMethod.databaseConfig(queryFindLanguagesByCountryName);
+            preparedStatementFoRResultSet.setString(1, PropertiesUtilManager.getPropertiesValue("countryName1"));
+
+            return addDatFromDBToList(true);
+        } catch (SQLException | TimeoutException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Set<WorldLanguagesEntity> updateWorldLanguage() {
+        try {
+            update = true;
+            preparedStatementFoRExecuteUpdate = commonEntityMethod.databaseConfig(updateLanguageName);
+            preparedStatementFoRExecuteUpdate.setString(1, PropertiesUtilManager.getPropertiesValue("newLanguageName"));
+            preparedStatementFoRExecuteUpdate.setString(2, PropertiesUtilManager.getPropertiesValue("languageName"));
+            try {
+                int updateRow = preparedStatementFoRExecuteUpdate.executeUpdate();
+                LOG.info("" + updateRow);
+                if (updateRow > 0) {
+                    update = true;
+                    Set<WorldLanguagesEntity> set = getLanguageByName();
+
+                    LOG.info(set.toString());
+                    return set;
+                } else {
+                    LOG.info("Can't update, LANGUAGE to update not there");
+                    languages = new CopyOnWriteArraySet<>();
+                    return languages;
+                }
+            } catch (SQLIntegrityConstraintViolationException e) {
+                LOG.info("Can't update , DUPLICATE VALUE");
+                languages = new CopyOnWriteArraySet<>();
+                return languages;
+            }
+
+        } catch (SQLException | TimeoutException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Set<WorldLanguagesEntity> updateWorldLanguageStatus() {
+        try {
+            preparedStatementFoRExecuteUpdate = commonEntityMethod.databaseConfig(updateLanguageStatus);
+            if (Boolean.parseBoolean(PropertiesUtilManager.getPropertiesValue("newWorldLanguageStatus"))) {
+                preparedStatementFoRExecuteUpdate.setString(1, "T");
+            } else {
+                preparedStatementFoRExecuteUpdate.setString(1, "F");
+            }
+            preparedStatementFoRExecuteUpdate.setString(2, PropertiesUtilManager.getPropertiesValue("languageName"));
+            int updatedRow = preparedStatementFoRExecuteUpdate.executeUpdate();
+            if (updatedRow > 0) {
+                Set<WorldLanguagesEntity> set = getLanguageByName();
+                LOG.info(set.toString());
+                return set;
+            } else {
+                languages = new CopyOnWriteArraySet<>();
+                return languages;
+            }
+
+        } catch (SQLException | TimeoutException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private PreparedStatement preparedStatementFoRResultSet;
+    private PreparedStatement preparedStatementFoRExecuteUpdate;
+    private boolean added = false;
+
+    private Set<WorldLanguagesEntity> insertWorldLanguage() {
+        try {
+            preparedStatementFoRExecuteUpdate = commonEntityMethod.databaseConfig(insertLanguage);
+            LOG.info(PropertiesUtilManager.getPropertiesValue("countryName1"));
+            preparedStatementFoRExecuteUpdate.setString(1, commonEntityMethod.getCountryCodeByCountryName(PropertiesUtilManager.getPropertiesValue("countryName1")));
+            preparedStatementFoRExecuteUpdate.setString(2, PropertiesUtilManager.getPropertiesValue("languageName"));
+            preparedStatementFoRExecuteUpdate.setString(3, "F");
+            preparedStatementFoRExecuteUpdate.setDouble(4, 0.0);
+
+            Set<WorldLanguagesEntity> set = getCityByLanguageAndCountry(PropertiesUtilManager.getPropertiesValue("languageName"), PropertiesUtilManager.getPropertiesValue("countryName1"));
+            LOG.info("Testing : " + set);
+
+            if (set.size() == 0) {
+                preparedStatementFoRExecuteUpdate.executeUpdate();
+                added = true;
+                languages = getCityByLanguageAndCountry(PropertiesUtilManager.getPropertiesValue("languageName"), PropertiesUtilManager.getPropertiesValue("countryName1"));
+                LOG.info("Data inserted");
+            } else {
+                if (added) {
+                    added = false;
+                } else {
+                    LOG.info("LOGGING INSERTING FAILED, DUPLICATE ELEMENT : " + set);
+                    languages.clear();
+                }
+            }
+            return languages;
         } catch (SQLException | TimeoutException | InterruptedException | ExecutionException e) {
             e.printStackTrace();
             return null;
@@ -87,37 +210,38 @@ public class WorldLanguagesEntity implements Callable<Set<WorldLanguagesEntity>>
     }
 
     @Override
-    public Set<WorldLanguagesEntity> call() throws Exception {
-        switch (ENTITY_TRIGGER) {
-            case "FIND_ALL_LANGUAGES":
-                return getAllLanguages();
-            case "FIND_LANGUAGE_BY_NAME":
-                return getLanguageByName();
-            case "FIND_LANGUAGES_BY_COUNTRY":
-                return getLanguageByCountry();
-            //below cases needs DTO's to use, next push methods will be used
-            case "UPDATE_LANGUAGE_STATUS":
-            case "ADD_LANGUAGE":
-        }
-        return null;
+    public Set<WorldLanguagesEntity> call() {
+        return switch (ENTITY_TRIGGER) {
+            case "FIND_ALL_LANGUAGES" -> getAllLanguages();
+            case "FIND_LANGUAGE_BY_NAME" -> getLanguageByName();
+            case "FIND_LANGUAGES_BY_COUNTRY" -> getLanguageByCountry();
+            case "UPDATE_LANGUAGE_STATUS" -> updateWorldLanguageStatus();
+            case "UPDATE_LANGUAGE_NAME" -> updateWorldLanguage();
+            case "ADD_LANGUAGE" -> insertWorldLanguage();
+            default -> null;
+        };
     }
 
-    public Set<WorldLanguagesEntity> addDatFromDBToList(ResultSet set) throws SQLException {
+    public Set<WorldLanguagesEntity> addDatFromDBToList(boolean isResultSet) throws SQLException {
         languages = new ConcurrentSkipListSet<>();
-        while (set.next()) {
-            if (set.getObject(3).toString().charAt(0) == 'T')
-                languages.add(new WorldLanguagesEntity(set.getString(1), set.getString(2), true));
-            else
-                languages.add(new WorldLanguagesEntity(set.getString(1), set.getString(2), false));
+        if (isResultSet) {
+            ResultSet set = preparedStatementFoRResultSet.executeQuery();
+            while (set.next()) {
+                if (set.getObject(3).toString().charAt(0) == 'T')
+                    languages.add(new WorldLanguagesEntity(set.getString(1), set.getString(2), true));
+                else
+                    languages.add(new WorldLanguagesEntity(set.getString(1), set.getString(2), false));
+            }
         }
-
+        LOG.info(languages.toString());
         return languages;
     }
 
     @Override
     public String toString() {
-        return "country : " + country + " language : " + language + " is language official : " + languageStatus;
+        return "country : " + countryName + " language : " + languageName + " is   official : " + languageStatus;
     }
+
 }
 
 
