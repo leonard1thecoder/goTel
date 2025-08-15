5f55f7570b830eae17b609cffc5fd00228e33e34
Sizolwakhe Mthimunye <leonard1geek@gmail.com>
2025-08-15 14:55:57 +0200

    *adding method to determine the token and username for different membership uses *change access modifier to protected

--- src/main/java/org/airpenthouse/GoTel/util/CommonEntityMethod.java
+++ src/main/java/org/airpenthouse/GoTel/util/CommonEntityMethod.java
@@ -1,21 +1,26 @@
 package org.airpenthouse.GoTel.util;
 
+import org.airpenthouse.GoTel.entities.membership.MembershipEntity;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.concurrent.*;
+
 import static org.airpenthouse.GoTel.util.Connect.getInstance;
 
 public class CommonEntityMethod {
 
     private final String jdbcQueryFindCountryCode;
+    private final MembershipEntity membershipEntity;
 
     protected CommonEntityMethod() {
+        membershipEntity = new MembershipEntity();
         jdbcQueryFindCountryCode = PropertiesUtilManager.getPropertiesValue("jdbc.query.findCountryCodeByCountry");
     }
 
-    public String getCountryCodeByCountryName(String countryName) {
+    protected String getCountryCodeByCountryName(String countryName) {
         PreparedStatement ps;
         try {
             ps = databaseConfig(jdbcQueryFindCountryCode);
@@ -32,7 +37,26 @@ public class CommonEntityMethod {
         return null;
     }
 
-    public PreparedStatement databaseConfig(String query) throws ExecutionException, InterruptedException, TimeoutException, SQLException {
+    public boolean checkMemberShipStatusAndTokenMatch() {
+        var set = this.membershipEntity.getMemberByToken();
+        var set2 = this.membershipEntity.getMemberByUsername();
+        try {
+            if (set.size() == 1 && set2.size() == 1) {
+                var memberList = set.stream().toList();
+                var memberStatus = memberList.get(0).getMembershipStatus();
+                var memberList2 = set2.stream().toList();
+                return memberStatus == 1 && memberList2.get(0).getMemberToken().equals(memberList.get(0).getMemberToken());
+            } else {
+                throw new RuntimeException("Error getting token failed, due set size is not equal to 1");
+            }
+        } catch (NullPointerException e) {
+            Log.info("Loading to no member request");
+            return false;
+        }
+    }
+
+
+    protected PreparedStatement databaseConfig(String query) throws ExecutionException, InterruptedException, TimeoutException, SQLException {
         Future<Connection> connect = getInstance().getDB_data();
         assert connect != null;
         return connect.get(15, TimeUnit.SECONDS).prepareStatement(query);
