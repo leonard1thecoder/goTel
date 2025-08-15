5a0b808aff1a46717b7c4556d08af5d54cbeadcf
Sizolwakhe Mthimunye <leonard1geek@gmail.com>
2025-08-15 15:08:33 +0200

    added abstract method to map from UpdateMembershipStatus to entity

--- src/main/java/org/airpenthouse/GoTel/util/mappers/MembershipMapper.java
+++ src/main/java/org/airpenthouse/GoTel/util/mappers/MembershipMapper.java
@@ -2,6 +2,7 @@ package org.airpenthouse.GoTel.util.mappers;
 
 import org.airpenthouse.GoTel.dtos.membership.MembershipRequest;
 import org.airpenthouse.GoTel.dtos.membership.RegisterMemberRequest;
+import org.airpenthouse.GoTel.dtos.membership.UpdateMembershipStatus;
 import org.airpenthouse.GoTel.dtos.membership.UpdateTokenRequest;
 import org.airpenthouse.GoTel.entities.membership.MembershipEntity;
 import org.mapstruct.Mapper;
@@ -18,6 +19,9 @@ public interface MembershipMapper {
     @Mapping(target = "registeredDate", expression = "java(java.time.LocalDateTime.now())")
     MembershipEntity toEntity(RegisterMemberRequest request);
 
+    @Mapping(target = "memberToken", expression = "java(org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric(7432))")
+    @Mapping(target = "registeredDate", expression = "java(java.time.LocalDateTime.now())")
+    MembershipEntity toEntity(UpdateMembershipStatus request);
     @Mapping(target = "memberToken", expression = "java(org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric(7432))")
     @Mapping(target = "registeredDate", expression = "java(java.time.LocalDateTime.now())")
     MembershipEntity toEntity(UpdateTokenRequest request);
