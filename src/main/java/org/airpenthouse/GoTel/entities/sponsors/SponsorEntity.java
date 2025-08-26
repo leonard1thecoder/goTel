dc064ba8bbd2e8e2682f86976855af54cb315671
Sizolwakhe Mthimunye <leonard1geek@gmail.com>
2025-08-26 17:51:49 +0200

    add sponsors entity

---
+++ src/main/java/org/airpenthouse/GoTel/entities/sponsors/SponsorEntity.java
@@ -0,0 +1,141 @@
+package org.airpenthouse.GoTel.entities.sponsors;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.airpenthouse.GoTel.util.CommonEntityMethod;
+import org.airpenthouse.GoTel.util.PropertiesUtilManager;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.*;
+
+@AllArgsConstructor
+public class SponsorEntity extends CommonEntityMethod implements Callable<List<SponsorEntity>> {
+
+    @Getter
+    private String sponsorName, SponsorSurname, sponsorEmailAddress, sponsorCellphoneNumber,
+            sponsorReasonToDonate;
+
+    @Getter
+    private String cardNumber, cvv, cardDate;
+    @Getter
+    private Double dononationAmount;
+
+    @Getter
+    private LocalDateTime paymentDate;
+
+    private String jdbcQueryGetAllSponsor, jdbcQueryGetSponsorByName, jdbcQueryInsertSponsor;
+
+    private PreparedStatement prepareResultSet, prepareInsert;
+
+    public static String EntityHandler;
+
+    public SponsorEntity() {
+        this.jdbcQueryInsertSponsor = PropertiesUtilManager.getPropertiesValue("jdbc.query.donate");
+        this.jdbcQueryGetAllSponsor = PropertiesUtilManager.getPropertiesValue("jdbbc.query.getSponsorByName");
+        this.jdbcQueryGetSponsorByName = PropertiesUtilManager.getPropertiesValue("jdbc.query.getAllSponsors");
+    }
+
+
+    private SponsorEntity(String sponsorName
+            , String sponsorSurname
+            , String sponsorEmailAddress
+            , String sponsorCellphoneNumber
+            , String sponsorReasonToDonate
+            , String cardNumber
+            , String cvv
+            , String cardDate
+            , Double dononationAmount
+            , LocalDateTime paymentDate) {
+        this.sponsorName = sponsorName;
+        SponsorSurname = sponsorSurname;
+        this.sponsorEmailAddress = sponsorEmailAddress;
+        this.sponsorCellphoneNumber = sponsorCellphoneNumber;
+        this.sponsorReasonToDonate = sponsorReasonToDonate;
+        this.cardNumber = cardNumber;
+        this.cvv = cvv;
+        this.cardDate = cardDate;
+        this.dononationAmount = dononationAmount;
+        this.paymentDate = paymentDate;
+    }
+
+    @Override
+    public List<SponsorEntity> call() throws Exception {
+
+        return switch (SponsorEntity.EntityHandler) {
+            case "GET_ALL_SPONSOR" -> getAllSponsors();
+            case "GET_SPONSOR_BY_NAME" -> getSponsorByName();
+            case "DONATE" -> insertDonate();
+            default -> throw new RuntimeException("Entity handler failed due to incorrect value");
+        };
+    }
+
+    private List<SponsorEntity> getAllSponsors() {
+        try {
+            prepareResultSet = databaseConfig(this.jdbcQueryGetAllSponsor);
+            return resultSetData(prepareResultSet.executeQuery());
+        } catch (ExecutionException | InterruptedException | TimeoutException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<SponsorEntity> getSponsorByName() {
+        try {
+            prepareResultSet = databaseConfig(this.jdbcQueryGetSponsorByName);
+            prepareResultSet.setString(1, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+
+            return resultSetData(prepareResultSet.executeQuery());
+        } catch (ExecutionException | InterruptedException | TimeoutException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<SponsorEntity> insertDonate() {
+        try {
+            prepareInsert = databaseConfig(this.jdbcQueryInsertSponsor);
+            prepareInsert.setString(1, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            prepareInsert.setString(2, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            prepareInsert.setString(3, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            prepareInsert.setString(4, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            prepareInsert.setString(5, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            prepareInsert.setString(6, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            prepareInsert.setString(7, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            prepareInsert.setString(8, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            prepareInsert.setString(9, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            prepareInsert.setString(10, PropertiesUtilManager.getPropertiesValue("sponsorsName"));
+            var entity = getSponsorByName();
+            if (entity.isEmpty()) {
+                prepareInsert.executeUpdate();
+                return getSponsorByName();
+            } else {
+                return null;
+            }
+        } catch (ExecutionException | InterruptedException | TimeoutException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private List<SponsorEntity> resultSetData(ResultSet set) throws SQLException {
+        var entities = new CopyOnWriteArrayList<SponsorEntity>();
+
+        while (set.next()) {
+            entities.add(new SponsorEntity(set.getString(1)
+                    , set.getString(2)
+                    , set.getString(3)
+                    , set.getString(4)
+                    , set.getString(5)
+                    , set.getString(6)
+                    , set.getString(7)
+                    , set.getString(8)
+                    , set.getDouble(9)
+                    , LocalDateTime.parse(set.getString(10))));
+        }
+        return entities;
+    }
+
+}
