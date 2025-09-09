1ee06510e5d2847465d566bf14905aeed7bfaef7
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2025-09-09 18:56:25 +0200

    removing the use of dto in controller

--- src/main/java/org/airpenthouse/GoTel/controllers/CountApiUsersController.java
+++ src/main/java/org/airpenthouse/GoTel/controllers/CountApiUsersController.java
@@ -1,9 +1,6 @@
 package org.airpenthouse.GoTel.controllers;
 
-import org.airpenthouse.GoTel.dtos.countApiUsers.CountApiUsersRequest;
 import org.airpenthouse.GoTel.util.CountApiUsers;
-import org.airpenthouse.GoTel.util.mappers.CountApiUsersMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +14,10 @@ import java.util.List;
 @CrossOrigin(origins="http://localhost:4200")
 public class CountApiUsersController extends CountApiUsers {
 
-    @Autowired
-    private CountApiUsersMapper mapper;
-
 
     @GetMapping("/")
-    public ResponseEntity<List<CountApiUsersRequest>> getAllCounts() {
-        var entities = prepareToGetAllCounties().stream().map(mapper::toDto).toList();
+    public ResponseEntity<List<CountApiUsers>> getAllCounts() {
+        var entities = prepareToGetAllCounties();
         if (entities.isEmpty()) {
             return ResponseEntity.notFound().build();
         } else {
